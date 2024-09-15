a87017b09634d8b237401925295c1966c16f7d36
seohyun <cheshire99.dev@gmail.com>
2024-09-16 05:43:02 +0900

    :sparkles: uri수정 및 독서기록 존재여부 조회

--- src/main/java/com/boggle_boggle/bbegok/repository/ReadingRecordRepository.java
+++ src/main/java/com/boggle_boggle/bbegok/repository/ReadingRecordRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ReadingRecordRepository extends JpaRepository<ReadingRecord, Long> {
-    Optional<ReadingRecord> findByUserAndBook(User user, Book book);
+    ReadingRecord findByUserAndBook(User user, Book book);
 }
