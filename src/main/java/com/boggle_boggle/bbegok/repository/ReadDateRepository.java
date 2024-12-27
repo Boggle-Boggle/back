6ba2bd78030c302502a94de37a30a882d1e9bdaf
seohyun <cheshire99.dev@gmail.com>
2024-12-27 22:19:16 +0900

    :sparkles: 회독조회 API 구현

--- src/main/java/com/boggle_boggle/bbegok/repository/ReadDateRepository.java
+++ src/main/java/com/boggle_boggle/bbegok/repository/ReadDateRepository.java
@@ -4,8 +4,11 @@ import com.boggle_boggle.bbegok.entity.ReadDate;
 import com.boggle_boggle.bbegok.entity.ReadingRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReadDateRepository extends JpaRepository<ReadDate, Long> {
     Optional<ReadDate> findByreadDateSeqAndReadingRecord(Long id, ReadingRecord readingRecord);
+
+    List<ReadDate> findByReadingRecordOrderByReadDateSeq(ReadingRecord readingRecord);
 }
